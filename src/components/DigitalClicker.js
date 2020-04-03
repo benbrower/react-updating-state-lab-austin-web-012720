ad034a36a8724e135ac32e38a79ea7c5cb0be893
benbrower <bbrower@smu.edu>
2020-04-02 20:28:29 -0400

    done

--- src/components/DigitalClicker.js
+++ src/components/DigitalClicker.js
@@ -1,1 +1,30 @@
 // Code DigitalClicker Component Here
+import React, { Component } from "react";
+
+export default class DigitalClicker extends Component {
+  constructor() {
+    super();
+    this.state = {
+      timesClicked: 0
+    };
+  }
+
+  handleClick = () => {
+    this.setState(
+      previousState => {
+        return {
+          timesClicked: previousState.timesClicked + 1
+        };
+      },
+      () => console.log(this.state.timesClicked)
+    );
+  };
+
+  render() {
+    return (
+      <button label={this.state.timesClicked} onClick={this.handleClick}>
+        Clicks: {this.state.timesClicked}
+      </button>
+    );
+  }
+}
--- src/components/YouTubeDebugger.js
+++ src/components/YouTubeDebugger.js
@@ -1,1 +1,53 @@
 // Code YouTubeDebugger Component Here
+import React, { Component } from "react";
+
+export default class YouTubeDebugger extends Component {
+  constructor() {
+    super();
+    this.state = {
+      errors: [],
+      user: null,
+      settings: {
+        bitrate: 8,
+        video: {
+          resolution: "1080p"
+        }
+      }
+    };
+  }
+
+  handleClick = event => {
+    if (event.target.className === "bitrate") {
+      this.setState({
+        settings: {
+          ...this.state.settings,
+          bitrate: 12
+        }
+      });
+    }
+    if (event.target.className === "resolution") {
+        this.setState({
+          settings: {
+            ...this.state.settings,
+            video: {
+                ...this.state.settings.video,
+                resolution: '720p'
+            }
+          }
+        });
+      }
+  };
+
+  render() {
+    return (
+      <div>
+        <button className="bitrate" onClick={this.handleClick}>
+          Current Bitrate: {this.state.settings.bitrate}
+        </button>
+        <button className="resolution" onClick={this.handleClick}>
+          Current Resolution: {this.state.settings.video.resolution}
+        </button>
+      </div>
+    );
+  }
+}
